91c161597adb9ce9f7941eb5b1ab6bbd059469fc
Sergei_Sharko <vulfer_d@mail.ru>
2023-12-04 23:48:04 +0300

    Add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+// Пример
+// “Hello my world” => “world my Hello”
+using System.Text;
+
+Console.InputEncoding = Encoding.Unicode;
+Console.OutputEncoding = Encoding.Unicode;
+
+Console.Write($"Введите строку, в которой слова будут расположены в обратном порядке -> ");
+string? input = Console.ReadLine();
+string[]? arrayWords = input?.Split(' ');
+Array.Reverse(arrayWords!);
+Console.WriteLine();
+Console.Write($"{input} => [{string.Join(" ", arrayWords!)}]");
